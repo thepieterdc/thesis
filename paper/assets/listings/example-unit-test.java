93593ba2a7ff74ad424a468d05cd52a6622d09c4
Pieter De Clercq <pieterdeclercq@outlook.com>
2020-05-30 17:38:56 +0200

    Fixed coverage images

--- paper/assets/listings/example-unit-test.java
+++ paper/assets/listings/example-unit-test.java
@@ -2,9 +2,8 @@ public class ExampleUnitTest {
   static int square(int base) { return base * base; }
 
   @Test
-  void testSquare() {
+  public void testSquare() {
     Assert.assertEquals(25, square(5));
     Assert.assertEquals(4, square(-2));
-    Assert.assertEquals(0, square(0));
   }
 }
