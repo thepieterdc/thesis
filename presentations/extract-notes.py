6438e51387b5023cc1d84adadbc7608ff0814323
Pieter De Clercq <pieterdeclercq@outlook.com>
2020-06-13 18:57:47 +0200

    Add speaker notes

---
+++ presentations/extract-notes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Verify arguments.
+import sys
+
+in_file, out_file = sys.argv[1:3]
+
+# Parse markdown.
+notes = []
+with open(in_file, "r") as fh:
+    slide_notes = []
+    for line in fh.readlines():
+        if line.startswith('---'):
+            notes.append(slide_notes)
+            slide_notes = []
+
+        elif line.startswith('<!-- Note:'):
+            slide_notes.append(line[10:].rstrip().rstrip("-->").strip())
+
+# Write file.
+with open(out_file, "w+") as fh:
+    fh.write("[notes]\n")
+    for slide, slide_notes in enumerate(notes):
+        if slide_notes:
+            fh.write(f"### {slide + 1}\n")
+            for note in slide_notes:
+                fh.write(f"{note}\n")
